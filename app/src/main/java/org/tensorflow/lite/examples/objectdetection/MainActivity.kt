93f8a9d1bc91f969c3884c7b11130f345012d492
Rishi0812 <rishiramesh08@gmail.com>
2023-01-29 08:07:40 +0530

    Added Navigation, Working

--- app/src/main/java/org/tensorflow/lite/examples/objectdetection/MainActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/objectdetection/MainActivity.kt
@@ -3,6 +3,7 @@ package org.tensorflow.lite.examples.objectdetection
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.speech.RecognitionListener
@@ -35,6 +36,16 @@ class MainActivity : AppCompatActivity() {
         context=this
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+        var tts: TextToSpeech? = null
+        //Initiate TTS
+        tts = TextToSpeech(context, TextToSpeech.OnInitListener { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                tts?.language = Locale.US
+                // Show the welcoming message
+                tts?.speak("Welcome to RAYS, Here are the List of commands: Start Detection, Stop Detection, Navigate. How may I help you?", TextToSpeech.QUEUE_FLUSH, null, null)
+                startListening()
+            }
+        })
 
         // Initialize the speech recognizer
         recognizer = SpeechRecognizer.createSpeechRecognizer(this)
@@ -56,6 +67,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResults(results: Bundle?) {
                 val result = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                var destination: String? = ""
                 if (result != null && result.isNotEmpty()) {
                     val spokenText = result[0]
                     Toast.makeText(context, spokenText, Toast.LENGTH_SHORT).show()
@@ -63,6 +75,9 @@ class MainActivity : AppCompatActivity() {
                         startDetection()
                     } else if (spokenText.contains("stop detection")) {
                         stopDetection()
+                    }else if (spokenText.contains("navigate to")) {
+                        destination = spokenText.substringAfter("navigate to").trim()
+                        startNavigation(destination)
                     }else{
                         startListening()
                     }
@@ -75,17 +90,6 @@ class MainActivity : AppCompatActivity() {
 
             override fun onPartialResults(partialResults: Bundle?) {}
             override fun onEvent(eventType: Int, params: Bundle?) {}
-        })
-
-        var tts: TextToSpeech? = null
-        //Initiate TTS
-        tts = TextToSpeech(context, TextToSpeech.OnInitListener { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                tts?.language = Locale.US
-                // Show the welcoming message
-                tts?.speak("Welcome to RAYS, How may I help you?", TextToSpeech.QUEUE_FLUSH, null, null)
-                startListening()
-            }
         })
     }
 
@@ -113,6 +117,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun startNavigation(destination: String?) {
+        if (!destination.isNullOrEmpty()) {
+            val gmmIntentUri = Uri.parse("google.navigation:q=$destination + \"&mode=w\"")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        } else {
+            // Handle the case where the destination is not set
+            Toast.makeText(this, "Destination not set", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
